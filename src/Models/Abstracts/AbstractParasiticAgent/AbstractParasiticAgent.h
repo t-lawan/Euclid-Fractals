aa2afb707a6920cde41242081d57ee0b86a7bcf6
akinsola <thomas.lawanson@gmail.com>
2020-11-24 11:13:42 +0000

    created abstract parasitic agent

---
+++ src/Models/Abstracts/AbstractParasiticAgent/AbstractParasiticAgent.h
@@ -0,0 +1,12 @@
+//
+//  AbstractParasiticAgent.h
+//  Euclid Fractals
+//
+//  Created by Thomas Lawanson on 24/11/2020.
+//
+
+#ifndef AbstractParasiticAgent_h
+#define AbstractParasiticAgent_h
+
+
+#endif /* AbstractParasiticAgent_h */
