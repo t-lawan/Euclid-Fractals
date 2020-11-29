835c489b9b0309290412d4f790303eb53524d609
akinsola <thomas.lawanson@gmail.com>
2020-11-29 22:21:33 +0000

    increased reproduction rate

--- src/Models/PlantDestroyer/PlantDestroyer.h
+++ src/Models/PlantDestroyer/PlantDestroyer.h
@@ -19,8 +19,8 @@ class PlantDestroyer : public AbstractMovingAgent {
     public:
         PlantDestroyer(ofVec2f _position, DNA _dna) : AbstractMovingAgent(_position, _dna){
             colour = ofColor::red;
-            MIN_REPRODUCTION_RATE = 0.000075;
-            MAX_REPRODUCTION_RATE = 0.0001;
+            MIN_REPRODUCTION_RATE = 0.0001;
+            MAX_REPRODUCTION_RATE = 0.001;
             MIN_MUTATION_RATE = 0.1;
             MAX_MUTATION_RATE = 0.2;
             MIN_SIZE = 10;
